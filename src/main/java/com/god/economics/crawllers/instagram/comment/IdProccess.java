1ecd7a6c2c96a805f5232c0cad3e266b3615927c
Lusics Mayrics <abbfzl@gmail.com>
2020-01-26 09:43:15 +0330

    started.............

--- src/main/java/com/god/economics/crawllers/instagram/comment/IdProccess.java
+++ src/main/java/com/god/economics/crawllers/instagram/comment/IdProccess.java
@@ -17,19 +17,27 @@ public class IdProccess {
 
 
     public static void main(String[] args) throws IOException {
-        FileWriter writer = new FileWriter("usernames.txt", false);
+        FileWriter writer = new FileWriter("usernames.txt", true);
 
         Scanner scanner = new Scanner(new File("ids.txt"));
+        int ap = 0;
         while (scanner.hasNext()) {
             String id = scanner.nextLine();
+            if (ap < 450) {
+                ap++;
+                continue;
+            }
+
+
             try {
-               String resp = Reqs.getReq("https://www.instagram.com/graphql/query/?query_hash=c9100bf9110dd6361671f113dd02e7d6&variables=%7B%22user_id%22%3A%22" + id + "%22%2C%22include_chaining%22%3Atrue%2C%22include_reel%22%3Atrue%2C%22include_suggested_users%22%3Afalse%2C%22include_logged_out_extras%22%3Afalse%2C%22include_highlight_reels%22%3Atrue%2C%22include_related_profiles%22%3Afalse%7D");
+                String resp = Reqs.getReq("https://www.instagram.com/graphql/query/?query_hash=c9100bf9110dd6361671f113dd02e7d6&variables=%7B%22user_id%22%3A%22" + id + "%22%2C%22include_chaining%22%3Atrue%2C%22include_reel%22%3Atrue%2C%22include_suggested_users%22%3Afalse%2C%22include_logged_out_extras%22%3Afalse%2C%22include_highlight_reels%22%3Atrue%2C%22include_related_profiles%22%3Afalse%7D");
                 JSONObject jsonObject = new JSONObject(resp);
                 String username = (String) ((JSONObject) ((JSONObject) ((JSONObject) ((JSONObject) jsonObject.get("data")).get("user")).get("reel"))
                         .get("user")).get("username");
 
-                writer.write(username + "\r\n");
                 System.out.println(username);
+                writer.write(username + "\r\n");
+                writer.flush();
             } catch (IOException e) {
                 e.printStackTrace();
             }
